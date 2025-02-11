df9fc42aa309afde76607cb49f9cc5eac79c7987
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2025-02-11 08:40:46 -0800

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect } from "react"
 
 function App() {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const API_URL = "https://api.themoviedb.org/3"
+
+  const fetchMovies = async () => {
+    const data = await axios.get(`${API_URL}/discover/movie`, {
+      params: {
+        api_key: import.meta.env.REACT_APP_MOVIE_API_KEY
+      }
+    })
+    console.log('data', data);
+  }
+
+    useEffect(() => {
+      fetchMovies();
+    }, [])
+
+    return (
+      <div>App</div>
+    )
+  }
+
+  export default App;
