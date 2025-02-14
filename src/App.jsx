206d1284233fd03be4d063fa47b182b014059c56
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2025-02-14 14:31:57 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import MovieCard from "./Components/MovieCard"
+import './App.css'
 
 function App() {
 
   const API_URL = "https://api.themoviedb.org/3"
+  const [movies, setMovies] = useState([])
 
   const fetchMovies = async () => {
-    const data = await axios.get(`${API_URL}/discover/movie`, {
+    const {data: {results}} = await axios.get(`${API_URL}/discover/movie`, {
       params: {
-        api_key: import.meta.env.REACT_APP_MOVIE_API_KEY
+        api_key: import.meta.env.VITE_MOVIE_API_KEY
       }
     })
-    console.log('data', data);
-  }
 
+   setMovies(results)
+  }
+  
     useEffect(() => {
       fetchMovies();
     }, [])
 
+
+    const renderMovies = () => (
+      movies.map(movie => (
+        <MovieCard
+        key={movie.id}
+        movie={movie}
+        />
+      ))
+    )
+
     return (
-      <div>App</div>
+      <div className="App">
+        <div className="Container">
+          {renderMovies()}
+        </div>
+      </div>
     )
   }
 
-  export default App;
+  export default App
---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MovieCard({movie}) {
+    const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+  return (
+    <div className={"movie-card"}>
+        {movie.poster_path ? <img className={"movie-cover"} src={`${IMAGE_PATH}${movie.poster_path}`} alt={movie.title} /> : null }
+        <h5 className={"movie-title"}>{movie.title}</h5>
+    </div>
+  )
+}
+
+export default MovieCard
